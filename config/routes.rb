f507b35f707ae4b0391d3f069391f6066c6a71e3
BetulTerzioglu <betulterzioglu01@gmail.com>
2023-08-10 11:57:03 +0300

    yayınla

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get "about", to:"about#index"
+  get "about-us", to:"about#index", as: :about
 
   get "sign_up", to:"registrations#new"
   post "users", to: "registrations#create"
+
+  get "try", to:"try#show"
 
   root to:"main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
